c8c9d5b1e04b64f56c2476e3c42b340ec54272d9
刘丽娜 <liulina@apps-MacBook-Pro.local>
2022-06-23 10:58:22 +0800

    修改目录结构

--- rollup.extends.config.ts
+++ rollup.extends.config.ts
@@ -4,20 +4,20 @@
  * @Author: liulina
  * @Date: 2022-06-23 09:21:07
  * @LastEditors: liulina
- * @LastEditTime: 2022-06-23 10:09:00
+ * @LastEditTime: 2022-06-23 10:38:35
  */
 import { getPackagesInfoList } from './build/getPackageName';
 import { kitCommonPluginList } from './build/config/rollup.base.config';
 
 const timeStart = new Date().getTime();
-const kitsList = getPackagesInfoList('extendPackages/', '_', function (filePath) {
+const kitsList = getPackagesInfoList('src/extendPackages/', '_', function (filePath) {
   console.log('done', new Date().getTime() - timeStart);
 });
 
 export default [
   {
     input: {
-      index: 'extendPackages/index.ts',
+      index: 'src/extendPackages/index.ts',
       ...kitsList
     },
     output: [
--- rollup.package.config.ts
+++ rollup.package.config.ts
@@ -4,19 +4,19 @@
  * @Author: liulina
  * @Date: 2022-06-23 09:21:30
  * @LastEditors: liulina
- * @LastEditTime: 2022-06-23 10:08:15
+ * @LastEditTime: 2022-06-23 10:38:48
  */
 import { getPackagesInfoList } from './build/getPackageName.js';
 
 import { kitCommonPluginList } from './build/config/rollup.base.config';
 
 const timeStart = new Date().getTime();
-const kitsList = getPackagesInfoList('packages/', '_', function (filePath) {
+const kitsList = getPackagesInfoList('src/packages/', '_', function (filePath) {
   console.log('done', new Date().getTime() - timeStart);
 });
 module.exports = {
   input: {
-    index: 'packages/index.ts',
+    index: 'src/packages/index.ts',
     ...kitsList
   },
   output: [
