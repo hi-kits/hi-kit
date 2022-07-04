13ea0ce9a7f12e9eb98bfdb6334e845930303215
zhengfeijun <zhengfeijun.xian@haier.com>
2022-07-04 08:58:53 +0800

    调整switch组件

--- src/packages/switch/switch.ts
+++ src/packages/switch/switch.ts
@@ -5,33 +5,27 @@
  * @Copyright © 2022 hi-kits. All rights reserved.
  * @description
  */
-
+// 核心库
 import { HIElement, customElement, attr, html, observable } from 'hi-element';
+// 混入基础功能
+import { HIElementBase } from '../_mixins/hiElementBase';
+// 样式文件
 import { SwitchStyles as styles } from "./switch.style";
-
+// 模版文件
 const template = html<HiSwitch>`
 <input type="checkbox" id="switch" :value="${x => x.initialValue}" ?disabled="${x => x.disabled}" />
 <label for="switch"></label>
 `;
+// 定义元素
 @customElement({
    name: 'h-switch',
    template,
    styles
 })
-export class HiSwitch extends HIElement {
-    
-    /**
-     * 无效
-     * @public  boolean
-     */
-    @attr disabled: boolean;
-    private disabledChanged(oldValue, newValue): void {
-        if(newValue!==null){
-            this.setAttribute('disabled', 'disabled');
-        }else{
-            this.removeAttribute('disabled');
-        }
-    }
+export class HiSwitch extends HIElementBase {
+    isfocus = true;
+    value;
+    switch;
     /**
      * When true, the control will be immutable by user interaction. See {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/readonly | readonly HTML attribute} for more information.
      * @public
@@ -47,14 +41,16 @@ export class HiSwitch extends HIElement {
             : this.classList.remove("readonly");
     }
 
-    @attr checked = true;
+    @attr checked;
     /**
      * @internal
      */
     public checkedChanged(prev: boolean | undefined, next: boolean) {
-        this.checked 
-            ? this.classList.add("checked") 
-            : this.classList.remove("checked");
+        if(next === null || next === false){
+            this.removeAttribute('checked');
+        }else{
+            this.setAttribute('checked', '');
+        }
     }
     /**
      * 选中时在表单提交中的元素值。
@@ -79,8 +75,43 @@ export class HiSwitch extends HIElement {
      * @internal
      */
      connectedCallback() {
-        super.connectedCallback()
-        this.shadowRoot!.addEventListener("click", this.clickHandler);
+        super.connectedCallback();
+        this.switch = this.shadowRoot!.getElementById('switch');
+        this.switch.addEventListener("click", this.clickHandler);
+        this.switch.addEventListener('change',(ev)=>{
+            this.checked = this.switch.checked;
+            this.$emit('change', {
+                checked: this.checked
+            });
+        });
+        this.switch.addEventListener('keydown', (ev) => {
+            switch (ev.keyCode) {
+                case 13://Enter
+                    this.checked = !this.checked;
+                    break;
+                default:
+                    break;
+            }
+        });
+        this.switch.addEventListener('focus',(ev)=>{
+            ev.stopPropagation();
+            if(!this.isfocus){
+                this.$emit('focus', {
+                    checked: this.value
+                });
+            }
+        });
+        this.switch.addEventListener('blur',(ev)=>{
+            ev.stopPropagation();
+            if(Number(getComputedStyle(this).zIndex) == 2 ){
+                this.isfocus = true;
+            }else{
+                this.isfocus = false;
+                this.$emit('blur', {
+                    checked: this.value
+                });
+            }
+        })
     }
 
 }
