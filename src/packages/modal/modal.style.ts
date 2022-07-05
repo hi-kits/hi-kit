36d26c805cac4c2b0b87ebf0f3c28b469dc29e1b
zhengfeijun <zhengfeijun.xian@haier.com>
2022-07-05 16:06:16 +0800

    调整modal组件

---
+++ src/packages/modal/modal.style.ts
@@ -0,0 +1,110 @@
+/**
+ * @const: ModalStyles
+ * @version 0.0.1
+ * @author by fico on 2022/04/27
+ * @Copyright © 2022 hi-kits. All rights reserved.
+ * @description
+ */
+
+import { css } from 'hi-element';
+import { hiConfig } from "../config";
+
+export const ModalStyles = css`
+
+:host{
+    position:fixed;
+    display:flex;
+    left:0;
+    top:0;
+    right:0;
+    bottom:0;
+    z-index:-1;
+    background:rgba(0,0,0,.3);
+    visibility:hidden;
+    opacity:0;
+    /* backdrop-filter: blur(3px); */ 
+    transition:.3s;
+}
+:host([open]){
+    opacity:1;
+    z-index:50;
+    visibility:visible;
+}
+.Modal {
+    display:flex;
+    position:relative;
+    min-width: 360px;
+    margin:auto;
+    box-shadow: 0px 11px 15px -7px rgba(0, 0, 0, 0.2), 0px 24px 38px 3px rgba(0, 0, 0, 0.14), 0px 9px 46px 8px rgba(0, 0, 0, 0.12);
+    box-sizing: border-box;
+    max-width: calc(100vw - 20px);
+    max-height: calc(100vh - 20px);
+    border-radius: 3px;
+    background-color: #fff;
+    opacity:0;
+    transform:scale(0.5);
+    transition:.3s cubic-bezier(.645, .045, .355, 1);
+}
+.ModalContent{
+    box-sizing: border-box;
+    display:flex;
+    width: 100%;
+    padding:0 20px;
+    flex:1;
+    flex-direction:column;
+}
+:host([open]) .Modal{
+    opacity:1;
+    transform:scale(1);
+}
+.ModalTitle {
+    line-height: 30px;
+    padding: 15px 30px 0 0;
+    font-weight: 700;
+    font-size: 14px;
+    color: #4c5161;
+    user-select: none;
+    cursor: default;
+}
+.ModalBody {
+    flex: 1;
+    overflow: auto;
+    min-height: 50px;
+    padding: 10px 0;
+}
+.ModalFooter {
+    padding: 3px 0 20px 0;
+    margin-top: -3px;
+    text-align: right;
+}
+.ModalClose{
+    position:absolute;
+    right:10px;
+    top:10px;
+    border:0;
+}
+.ModalFooter h-button {
+    margin-left:10px;
+}
+
+.ModalCancel{
+    visibility:hidden;
+}
+
+:host([type="confirm"]) .ModalCancel,
+:host([type="prompt"]) .ModalCancel{
+    visibility:visible;
+}
+h-input{
+    width:100%;
+}
+:host(:not(:empty)) h-input{
+    margin-top:10px;
+}
+:host(:empty) .dialog-body{
+    min-height:0;
+}
+
+`;
+ 
+
---
+++ src/packages/modal/modal.ts
@@ -0,0 +1,224 @@
+/**
+ * @class: HiModal 对话框
+ * @version 0.0.1
+ * @author by fico on 2022/04/27
+ * @Copyright © 2022 hi-kits. All rights reserved.
+ * @description
+ */
+
+import { HIElement, customElement, attr, html, ref, when, observable } from 'hi-element';
+import { ModalStyles as styles } from "./modal.style";
+
+
+/**
+ * 消息类型
+ */
+type ModalType = 'alert' | 'confirm' | 'prompt';
+
+/**
+ * 消息提示
+ * @function get
+ * @param { Object } options 请求参数对象
+ * @param { string } options.type 消息类型
+ * @param { string } options.content 消息内容
+ * @param { number } options.duration 自动关闭的延时，单位毫秒。设为 0 时不自动关闭
+ * @param { number } options.callback 关闭时触发的回调函数
+ */
+interface ModalOptions {
+    title?: string;
+    type?: ModalType;
+    content: string;
+    oktext: string;
+    ok?: () => void;
+    canceltext: string;
+    cancel?: () => void;
+}
+
+const template = html<HiModal>`
+<div class="Modal">
+    <div class="ModalContent">
+        <div class="ModalTitle">${x => x.title}</div>
+        <h-button 
+            class="ModalClose" 
+            @click="${(x, c) => x.close()}"
+        ><h-icon name="close" size="14"></h-icon>
+        </h-button>
+        <div class="ModalBody">
+            <slot></slot>
+            ${when(
+                x => x.type=="prompt",
+                html`<h-input></h-input>`
+            )}
+        </div>
+        <div class="ModalFooter">
+            <h-button class="ModalCancel"
+                @click="${(x, c) => x.close()}"
+            >${x => x.canceltext}</h-button>
+            <h-button class="ModalSubmit" type="primary"
+                @click="${(x, c) => x.okfn()}"
+            >${x => x.oktext}</h -button>
+        </div>
+    </div>
+</div>
+
+`;
+@customElement({
+   name: 'h-modal',
+   template,
+   styles
+})
+export class HiModal extends HIElement {
+    // ------------------ 构造函数 ------------------
+    constructor() {
+        super();
+    }
+    // ------------------ 参数 ------------------
+    ok = (() => {});
+    cancel = (() => {});
+    loading;
+    autoclose = true;
+    // ------------------ 属性 ------------------
+    
+    /**
+     * 取消键文本
+     * @date 7/5/2022 - 11:23:08 AM
+     *
+     * @public
+     * @type {!string}
+     */
+    @attr public canceltext: string = '取消';
+    private canceltextChanged(oldValue, newValue): void {
+        this.canceltext = newValue;
+    }
+    
+    /**
+     * 确定键文本
+     * @date 7/5/2022 - 11:23:18 AM
+     *
+     * @public
+     * @type {!string}
+     */
+    @attr public oktext:string = '确定';
+    private oktextChanged(oldValue, newValue): void {
+        this.oktext = newValue;
+    }
+    
+    /**
+     * 对话框类型
+     * @date 7/5/2022 - 11:08:04 AM
+     *
+     * @type {ModalType}
+     */
+    @attr type: ModalType;
+    
+    /**
+     * 标题
+     * @date 7/5/2022 - 11:09:40 AM
+     *
+     * @type {string}
+     */
+    @attr title: string;
+    
+    /**
+     * 显示open
+     * 当 HiModal 内容比较复杂时，可以直接写在页面body上，通过 open 属性来控制显示
+     * @date 7/5/2022 - 11:29:08 AM
+     *
+     * @type {boolean}
+     */
+    @attr({ mode: "boolean" }) open: boolean
+
+    // ------------------ 自定义函数 ------------------
+    /**
+     * 当自定义元素第一次被连接到文档DOM时被调用
+     * @internal
+     */
+    connectedCallback(): void {
+        super.connectedCallback()
+       
+    }
+    /**
+     * 关闭弹窗
+     */
+    close(): void {
+        this.cancel();
+        this.open = false;
+    }
+    okfn(): void {
+        this.dispatchEvent(new CustomEvent('submit'));
+        if(!this.loading && this.autoclose){
+            this.open = false;
+        }
+        this.ok();
+    }
+    /**
+     * 设置参数
+     * @date 2022-06-16
+     * @param { any } message
+     * @param { string} type
+     * @param { string} content
+     * @param { number } duration
+     * @param { callback: () => void = (() => {})} 
+     * @returns { void }
+     */
+     setParams (
+        modal:any, 
+        type: string | undefined, 
+        options: ModalOptions
+    ): void {
+        modal.type = type;
+        modal.open = true;
+        modal.ok = options.ok ?? modal.ok;
+        modal.cancel = options.cancel ?? modal.cancel;
+        modal.textContent = options.content;
+        modal.title = options.title;
+        if (type == 'prompt') {
+            modal.autoclose = false;
+        }
+    }
+}
+
+let modalContent: any = document.getElementById('HiModalWrap');
+if(!modalContent){
+    modalContent = document.createElement('div');
+    modalContent.id = 'HiModalWrap';
+    modalContent.style = 'position:fixed; left:0; right:0; top:10px; z-index:51;';
+    document.body.appendChild(modalContent);
+}
+
+export const HiModalService = {
+    /**
+     * 信息提示
+     * @date 2022-06-16
+     * @param { ModalOptions} options
+     */
+    alert: ( options: ModalOptions ) => {
+        const modal = new HiModal();
+        modalContent.appendChild(modal);
+        options.title = options.title || 'Alert';
+        modal.setParams(modal, 'alert', options);
+    },
+ 
+    /**
+     * 加载提示
+     * @date 2022-06-16
+     * @param { ModalOptions} options
+     */
+    confirm: ( options: ModalOptions ) => {
+        const modal = new HiModal();
+        modalContent.appendChild(modal);
+        options.title = options.title || 'Confirm';
+        modal.setParams(modal, 'confirm', options);
+    },
+    /**
+     * 通用提示
+     * @date 2022-06-16
+     * @param { ModalOptions} options
+     */
+    prompt: ( options: ModalOptions ) => {
+        const modal = new HiModal();
+        modalContent.appendChild(modal);
+        options.title = options.title || 'Prompt';
+        modal.setParams(modal, 'prompt', options);
+    },
+}
