a76fe8977bb7fff3a9c5327e27660dac5847c130
zhengfeijun <zhengfeijun.xian@haier.com>
2022-07-08 15:10:08 +0800

    调整组件

--- src/packages/navDot/nav.dot.ts
+++ src/packages/navDot/nav.dot.ts
@@ -26,7 +26,7 @@ const template = html<HiNavDot>`
             <li
                 class="${(x, c) => c.parent.current}"
                 @click="${(x, c) => c.parent.dotClick(c.parentContext.event as MouseEvent, c.index)}">${x => x}
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 16 16"preserveAspectRatio="none"><circle cx="8" cy="8" r="6.215"/></svg></li>
+                </li>
         `
     )}
 </ul>
